aed3c3cd151d3a833b91e6f199fa9a985fde7098
Hayat-07 <hayat1607207@gmail.com>
2024-02-01 20:02:27 +0600

    "5th commit"

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,9 @@ import { CiLocationOn } from "react-icons/ci";
 import { HiOutlineClipboardList } from "react-icons/hi";
 
 function Main() {
+
+
+  
   return (
     <section className="main container section">
       <div className="sectionTitle">
@@ -12,17 +15,18 @@ function Main() {
 
       <div className="sectContent grid">
         {data.map((x, i) => {
+          let descript=x.description.substring(0,100)
           return (
             <>
               <div key={i} className="cardMy grid">
                 <div className="imgDiv ">
                 <img src={x.img} alt="Image" />
                 </div>
-               <div style={{display:"flex", justifyContent:"center",alignItems:"center" }}>
-               <h2>{x.name}</h2> <span><CiLocationOn /></span>
+               <div style={{display:"flex", justifyContent:"center",alignItems:"center",padding:"3px" }}>
+               <h3 >{x.name} <CiLocationOn /></h3> 
                </div>
-                <h3>{x.fees}</h3>
-                <h6>{x.description}</h6>
+                <h4>{x.fees}</h4>
+                <h6>{descript}</h6>
                 <div className="btn flex" style={{color:"white", width:"50%",margin:"auto", height:"1.5rem",textAlign:"center"}}>Dtails <HiOutlineClipboardList />
  </div>
               </div>
